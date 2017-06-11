5c68369b530ed9ea1e48e2ee1d0f72163ed7f0b0
hapins <seanieboy10@gmail.com>
2017-06-10 21:34:55 -0400

    Bug fixes

--- twitter_interface.py
+++ twitter_interface.py
@@ -29,7 +29,7 @@ def get_trending(api):
 
 #Top n results for a given search parameter
 def get_tweets_text(api, search_term, count):
-    search_result = api.search(search_term, count=count)
+    search_result = api.search(search_term, count=count, lang="eng")
     tweets = []
     for status in search_result:
         tweets.append(status.text)
